da40bc2f05418e6286ca9baa9c8bba84a22168ac
dzherb <zherbin.dima@yandex.ru>
2025-04-01 19:19:44 +0300

    implement Cycle

--- itertools.go
+++ itertools.go
@@ -13,3 +13,27 @@ func Count(start, step int) iter.Seq[int] {
 		}
 	}
 }
+
+func Cycle[V any](iter iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for {
+			for v := range iter {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func Cycle2[K, V any](iter iter.Seq2[K, V]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for {
+			for k, v := range iter {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
--- itertools_test.go
+++ itertools_test.go
@@ -2,9 +2,39 @@ package itertools
 
 import (
 	"iter"
+	"maps"
 	"reflect"
+	"slices"
 	"testing"
 )
+
+func unwrapIterator[V any](iter iter.Seq[V], limit int) []V {
+	i := 0
+	res := make([]V, 0, limit)
+	for item := range iter {
+		if i >= limit {
+			break
+		}
+		res = append(res, item)
+		i++
+	}
+	return res
+}
+
+func unwrapIterator2[K, V any](iter iter.Seq2[K, V], limit int) ([]K, []V) {
+	i := 0
+	keys := make([]K, 0, limit)
+	val := make([]V, 0, limit)
+	for k, v := range iter {
+		if i >= limit {
+			break
+		}
+		keys = append(keys, k)
+		val = append(val, v)
+		i++
+	}
+	return keys, val
+}
 
 func TestCount(t *testing.T) {
 	type args struct {
@@ -64,15 +94,78 @@ func TestCount(t *testing.T) {
 	}
 }
 
-func unwrapIterator[T any](iter iter.Seq[T], limit int) []T {
-	i := 0
-	res := make([]T, 0, limit)
-	for item := range iter {
-		if i >= limit {
-			break
-		}
-		res = append(res, item)
-		i++
+func TestCycle(t *testing.T) {
+	type args[V any] struct {
+		iter iter.Seq[V]
 	}
-	return res
+	type testCase struct {
+		name  string
+		args  args[int]
+		limit int
+		want  []int
+	}
+	tests := []testCase{
+		{
+			name: "[1, 2, 3] cycle",
+			args: args[int]{
+				slices.Values([]int{1, 2, 3}),
+			},
+			limit: 9,
+			want:  []int{1, 2, 3, 1, 2, 3, 1, 2, 3},
+		},
+		{
+			name: "[-10, 0] cycle",
+			args: args[int]{
+				slices.Values([]int{-10, 0}),
+			},
+			limit: 5,
+			want:  []int{-10, 0, -10, 0, -10},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cycle := Cycle(tt.args.iter)
+			if got := unwrapIterator(cycle, tt.limit); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Cycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCycle2(t *testing.T) {
+	type args[K any, V any] struct {
+		iter iter.Seq2[K, V]
+	}
+	type testCase struct {
+		name       string
+		args       args[int, string]
+		limit      int
+		wantKeys   []int
+		wantValues []string
+	}
+	tests := []testCase{
+		{
+			name: "map cycle",
+			args: args[int, string]{
+				maps.All(map[int]string{2: "a", 4: "b", 6: "c"}),
+			},
+			limit:      9,
+			wantKeys:   []int{2, 4, 6, 2, 4, 6, 2, 4, 6},
+			wantValues: []string{"a", "b", "c", "a", "b", "c", "a", "b", "c"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cycle := Cycle2(tt.args.iter)
+			gotKeys, gotValues := unwrapIterator2(cycle, tt.limit)
+
+			if got := gotKeys; !reflect.DeepEqual(got, tt.wantKeys) {
+				t.Errorf("Cycle2() = %v, want %v", got, tt.wantKeys)
+			}
+
+			if got := gotValues; !reflect.DeepEqual(got, tt.wantValues) {
+				t.Errorf("Cycle2() = %v, want %v", got, tt.wantValues)
+			}
+		})
+	}
 }
